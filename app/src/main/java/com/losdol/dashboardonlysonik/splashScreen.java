a42a5f473257c4cd818058697f7f195da33f2d01
HiPeng-sange <51777997+HiPeng-sange@users.noreply.github.com>
2020-08-12 16:18:26 +0700

    Perdana

---
+++ app/src/main/java/com/losdol/dashboardonlysonik/splashScreen.java
@@ -0,0 +1,22 @@
+package com.losdol.dashboardonlysonik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashScreen.this,dashboard.class));
+            }
+        },2000);
+    }
+}
